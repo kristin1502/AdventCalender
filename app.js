7c6538aa3e2a5dab5b4dcfa2f6bd4406fa11c3ff
Kristin <kristin1502.github@gmail.com>
2024-11-30 18:27:06 +0100

    code added

---
+++ app.js
@@ -0,0 +1,8 @@
+/* Created by Tivotal 
+    customize by me */
+
+const wrapper = document.querySelector(".wrapper");
+
+wrapper.addEventListener("click", () => {
+  wrapper.classList.toggle("open");
+});
